81729f34f053568eae40b43263096e29009eb014
Joel Lord <jlord@macadamian.com>
2016-01-20 14:05:27 -0500

    Fixed a small bug

--- helpers.js
+++ helpers.js
@@ -3,9 +3,10 @@ module.exports = {
     return str.substr(0, 1).toUpperCase() + str.substr(1);
   },
   FormattedChartData: function(original) {
+    var self = this;
     var chartData = [];
     for (var key in original) {
-      chartData.push([capitalize(key), original[key]]);
+      chartData.push([self.Capitalize(key), original[key]]);
     }
     return chartData;
   }
